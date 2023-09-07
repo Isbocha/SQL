41057a6183b08ba34f6a3a1e03c7a8968cb99e38
InnaB <isbocha@gmail.com>
2023-09-07 20:52:41 +0300

    Initial commit

---
+++ src/test/java/ru/netology/sql/page/DashboardPage.java
@@ -0,0 +1,19 @@
+package ru.netology.sql.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.sql.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement header = $("[data-test-id=dashboard]");
+
+    public DashboardPage() {
+        header.shouldBe(visible);
+    }
+}
---
+++ src/test/java/ru/netology/sql/page/LoginPage.java
@@ -0,0 +1,32 @@
+package ru.netology.sql.page;
+
+import com.codeborne.selenide.Condition;
+
+import ru.netology.sql.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    public void verifyErrorNotification(String expectedText) {
+        $("[data-test-id=error-notification]").shouldHave(exactText(expectedText)).shouldBe(visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+
+        $("[data-test-id=login] .input__control").setValue(info.getLogin());
+        $("[data-test-id=password] .input__control").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+        return new VerificationPage();
+    }
+    public void invalidLogin(DataHelper.AuthInfo info) {
+
+        $("[data-test-id=login] .input__control").setValue(info.getLogin());
+        $("[data-test-id=password] .input__control").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+
+    }
+
+}
